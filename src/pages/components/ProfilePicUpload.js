e77e418cd08157b0181b567b5e89a92fdbeffc14
lokeshsatpute <satputelokesh01@gmail.com>
2023-11-27 00:26:08 -0500

    implemented GPS Config

---
+++ src/pages/components/ProfilePicUpload.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ProfilePicUpload() {
+    return ( 
+        <div>
+            <h3>Upload Profile Picture </h3>
+            <div className="button" style={{width: '100px'}} variant="primary" type="submit">
+                Upload
+            </div>
+        </div>
+    )
+}
